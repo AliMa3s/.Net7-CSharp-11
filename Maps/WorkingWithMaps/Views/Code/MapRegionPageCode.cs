d8c2377f0c1ca7205fc4fe67a6c83a820b5e1ab0
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2023-01-06 05:51:06 +0100

    Add files via upload

---
+++ Maps/WorkingWithMaps/Views/Code/MapRegionPageCode.cs
@@ -0,0 +1,22 @@
+﻿//using Microsoft.Maui.Maps;
+//using Map = Microsoft.Maui.Controls.Maps.Map;
+
+//namespace WorkingWithMaps.Views.Code;
+
+//public class MapRegionPageCode : ContentPage
+//{
+//    public MapRegionPageCode()
+//    {
+//        Title = "Map region demo";
+
+//        Location location = new Location(51.5071, 0.1276); ;
+//        MapSpan mapSpan = new MapSpan(location, 0.01, 0.01);
+//        //MapSpan mapSpan = MapSpan.FromCenterAndRadius(position, Distance.FromKilometers(0.444));
+
+//        Map map = new Map(mapSpan);
+
+//        StackLayout sl = new StackLayout { Margin = new Thickness(10) };
+//        sl.Add(map);
+//        Content = sl;
+//    }
+//}
