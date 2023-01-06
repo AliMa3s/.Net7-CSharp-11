c8f784711d7ce84079835192dcb884e3e163751b
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2023-01-06 05:48:45 +0100

    Add files via upload

---
+++ MinimalApiChanges/MinimalApiChanges/Model/ToDoItemRepository.cs
@@ -0,0 +1,33 @@
+﻿namespace MinimalApiChanges.Model
+{
+    public class ToDoItemRepository : IToDoItemRepository
+    {
+        private static List<ToDoItem>? _allToDoItems;
+
+        public List<ToDoItem>? GetAllToDoItems()
+        {
+            if (_allToDoItems == null)
+            {
+                InitializeData();
+            }
+
+            return _allToDoItems;
+        }
+
+        public void AddToDoItem(ToDoItem toDoItem)
+        {
+            _allToDoItems?.Add(toDoItem);
+        }
+
+        private void InitializeData()
+        {
+            _allToDoItems = new List<ToDoItem>()
+                {
+                    new ToDoItem(){Title = "Get milk", Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit.", Completed=true },
+                    new ToDoItem(){Title = "Learn Blazor", Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+                    new ToDoItem(){Title = "Create an app with Blazor", Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit.", Completed=true},
+                    new ToDoItem(){Title = "Put up Christmas tree", Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
+                };
+        }
+    }
+}
