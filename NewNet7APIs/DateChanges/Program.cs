6ddf095b0dc182765407f748515e2b055b2d9724
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2023-01-06 05:46:54 +0100

    Add files via upload

---
+++ NewNet7APIs/DateChanges/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Hit a key to start!");
+Console.ReadLine();
+var stopWatch = Stopwatch.StartNew();
+
+Console.WriteLine("Hit a key to stop!");
+Console.ReadLine();
+
+stopWatch.Stop();
+
+Console.WriteLine($"{stopWatch.Elapsed.TotalMilliseconds} ms");
+Console.WriteLine($"{stopWatch.Elapsed.TotalMicroseconds} µs");
+Console.WriteLine($"{stopWatch.Elapsed.TotalNanoseconds} ns");
+
+var verySoon = DateTime.Now.AddMicroseconds(100);
+
+Console.WriteLine(verySoon.ToLongTimeString());
+
+Console.ReadLine() ;
+
---
+++ NewNet7APIs/TarSupport/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Formats.Tar;
+
+var targetTarFile = @"D:\temp2\documents.tar";
+
+
+if (File.Exists(targetTarFile))
+{
+    File.Delete(targetTarFile);
+}
+
+TarFile.CreateFromDirectory(@"D:\temp", targetTarFile, false);
+
+
+var destination = @$"D:\temp\{Guid.NewGuid()}";
+Directory.CreateDirectory(destination);
+TarFile.ExtractToDirectory(targetTarFile, destination, true);
+
